31b33851507056372152fba0f582dcd5b22bf474
Israel Kusayev <israel.kusayev@gmail.com>
2020-02-20 22:38:09 +0200

    :bug: fix has filed middleware

--- src/api/middlewares/hasFieldMiddleware.ts
+++ src/api/middlewares/hasFieldMiddleware.ts
@@ -3,9 +3,9 @@ import { Errors } from '../../utils/errors';
 
 export default function hasFields<T>(keys: (keyof T)[]) {
   return function(req: Request, res: Response, next: NextFunction) {
-    keys.forEach(key => {
+    for (const key of keys)
       if (!req.body[key]) return res.status(400).json({ msg: Errors.FieldRequired.format(key.toString()) });
-    });
+
     next();
   };
 }
